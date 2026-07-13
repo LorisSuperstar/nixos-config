eab6a4beb8887ec656e88e4c58603c6e7c850ae7
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com.>
2026-07-13 21:36:38 +0200

    add ares

--- home-loris.nix
+++ home-loris.nix
@@ -41,6 +41,7 @@
       protonvpn-gui
       wireguard-tools
       vbam
+      ares
 
       # 2. Corrected virt-manager override: Wrapped in parentheses to evaluate as one item
       (virt-manager.overrideAttrs (oldAttrs: {
